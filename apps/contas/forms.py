a2882f16b6e195d86a90ca9d3bb1ab3163eecfb9
caio-tacito <caio.tacito@gmail.com>
2024-11-02 16:31:10 -0300

    Tratativa de Erros Revisão

--- apps/contas/forms.py
+++ apps/contas/forms.py
@@ -1,9 +1,15 @@
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.core.exceptions import ValidationError
-from contas.models import MyUser 
+import re
+import random # escolha aleatoria
+import string # contem todas as letras do alfabeto, etc.
 
-class CustomUserCreationForm(UserCreationForm):
+from django import forms
+from django.core.mail import send_mail
+from django.core.exceptions import ValidationError
+
+from contas.models import MyUser
+from core import settings
+
+class CustomUserCreationForm(forms.ModelForm):
     password1 = forms.CharField(label="Senha", widget=forms.PasswordInput) 
     password2 = forms.CharField(label="Confirmação de Senha", widget=forms.PasswordInput)
 
@@ -18,12 +24,31 @@ class CustomUserCreationForm(UserCreationForm):
         }
 
     def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        if self.user.is_authenticated:
+            del self.fields['password1']
+            del self.fields['password2']
         for field_name, field in self.fields.items():
             if field.widget.__class__ in [forms.CheckboxInput, forms.RadioSelect]:
                 field.widget.attrs['class'] = 'form-check-input'
             else:
                 field.widget.attrs['class'] = 'form-control'
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        if len(password1) < 8:
+            raise forms.ValidationError("A senha deve conter pelo menos 8 caracteres.")
+        
+        # Verifique se a senha contém pelo menos uma letra maiúscula, uma letra minúscula e um caractere especial
+        maiusculas=re.search(r'[A-Z]', password1)
+        minusculas=re.search(r'[a-z]', password1)
+        caract_esp=re.search(r'[!@#$%^&*(),.?":{}|<>]', password1)
+        if not maiusculas or not minusculas or not caract_esp:
+            raise forms.ValidationError("A senha deve conter \
+                                            pelo menos 8 caracteres, uma letra maiúscula, uma letra \
+                                            minúscula e um caractere especial.")
+        return password1
     
     def clean_password2(self):
         # Check that the two password entries match
@@ -36,11 +61,23 @@ class CustomUserCreationForm(UserCreationForm):
     def save(self, commit=True):
         # Save the provided password in hashed format
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        if self.user.is_authenticated:
+            password = ''.join(random.choices(string.digits, k=6)) # Gerar uma senha 
+            user.set_password(password) # salvo essa senha
+            user.force_change_password = True # força mudança de senha quando logar.
+            send_mail( # Envia email para usuario
+                'Sua senha provisória',
+                f'Sua senha provisório para entrar na plataforma é: {password}',
+                settings.DEFAULT_FROM_EMAIL, # De (em produção usar o e-mail que está no settings: settings.DEFAULT_FROM_EMAIL)
+                [user.email], # para
+                fail_silently=False,
+            )
+        else:
+            user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
         return user
-    
+        
 class UserChangeForm(forms.ModelForm):
     class Meta:
         model = MyUser
