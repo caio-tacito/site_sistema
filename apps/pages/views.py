d959e62047fdd919d4fc77e8a2f95317bc15ae66
brunoaraujo <brunoaalisboa@gmail.com>
2024-12-08 12:01:46 -0300

    Aula 107

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.contrib import messages
 
+from pages.models import Blocos
+
 # Create your views here.
 def index(request):
-    #count = 10
-    #messages.debug(request, "%s SQL statements were executed." % count)
-    #messages.info(request, "Three credits remain in your account.")
-    #messages.success(request, "Profile details updated.")
-    #messages.warning(request, "Your account expires in three days.")
-    #messages.error(request, "Document deleted.")
-
     return render(request, 'index.html')
+
+
+def paginas_view(request):
+    url_name = request.resolver_match.url_name
+    print(f'paginas_view -> url_name: {url_name}')
+    pagina = {
+        'home': Blocos.objects.filter(pagina__nome='inicio',ativo=True).order_by('ordem'),
+        'sobre': Blocos.objects.filter(pagina__nome='sobre',ativo=True).order_by('ordem'),
+        'faq': Blocos.objects.filter(pagina__nome='faq',ativo=True).order_by('ordem'),
+        'contato': Blocos.objects.filter(pagina__nome='contato',ativo=True).order_by('ordem'),
+        }
+    print(f'paginas_view -> pagina[str(url_name)]: {pagina[str(url_name)]}')
+    context = {'blocos': pagina[str(url_name)]}
+    return render(request, 'index.html', context)
